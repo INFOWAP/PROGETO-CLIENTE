fa2c021256256244dd9d68aef059378799957cc7
ADAOMASSO <adaomasso@gmail.com>
2021-02-20 00:09:58 -0300

    20210219-0010

--- cliente/src/app/cliente/cliente.module.ts
+++ cliente/src/app/cliente/cliente.module.ts
@@ -1,11 +1,18 @@
-import { TabelaComponent } from './tabela/tabela.component';
-import { FormularioComponent } from './formulario/formulario.component';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ClienteRoutingModule } from './cliente-routing.module';
-
+import { FormularioComponent } from './formulario/formulario.component';
+import { TabelaComponent } from './tabela/tabela.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,15 @@ import { ClienteRoutingModule } from './cliente-routing.module';
   ],
   imports: [
     CommonModule,
-    ClienteRoutingModule
+    BrowserAnimationsModule,
+    ClienteRoutingModule,
+    FormsModule,
+    DropdownModule,    
+    InputMaskModule,
+    FileUploadModule,
+    TableModule, 
+    ButtonModule, 
+    InputTextModule
   ]
 })
 export class ClienteModule { }
